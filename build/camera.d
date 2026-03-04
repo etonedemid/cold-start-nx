d341f23e57724b5cd9a2b137c679af8ce400a71e
etonedemid <etonedemid@gmail.com>
2026-03-04 21:29:45 +0000

    fix: remove committed build artifacts and add .gitignore
    The build-pc/ and build/ directories were committed with machine-specific
    hardcoded absolute paths that caused CI failures:
    - build-pc/CMakeCache.txt referenced /home/etonedemid/topdown-nx/cold_start/
    - build/*.d files referenced /home/etonedemid/topdown-nx/cold_start/source/
    
    Add .gitignore to prevent future accidental commits of build artifacts.
    
    Co-authored-by: etonedemid <130842728+etonedemid@users.noreply.github.com>

--- build/camera.d
+++
@@ -1,7 +0,0 @@
-camera.o: /home/etonedemid/topdown-nx/cold_start/source/camera.cpp \
- /home/etonedemid/topdown-nx/cold_start/source/camera.h \
- /home/etonedemid/topdown-nx/cold_start/source/vec2.h \
- /home/etonedemid/topdown-nx/cold_start/source/constants.h
-/home/etonedemid/topdown-nx/cold_start/source/camera.h:
-/home/etonedemid/topdown-nx/cold_start/source/vec2.h:
-/home/etonedemid/topdown-nx/cold_start/source/constants.h:
--- build/mappack.d
+++
@@ -1,3 +0,0 @@
-mappack.o: /home/etonedemid/topdown-nx/cold_start/source/mappack.cpp \
- /home/etonedemid/topdown-nx/cold_start/source/mappack.h
-/home/etonedemid/topdown-nx/cold_start/source/mappack.h:
