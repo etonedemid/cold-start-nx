73338f94a4d35bfd3f85cb5119cae18ebd0ea277
etonedemid <3damir0n3@gmail.com>
2026-03-06 22:33:48 -0300

    restore

---
+++ build-win/CMakeFiles/enet_bundled.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/enet_bundled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-win/CMakeFiles/enet_bundled.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for enet_bundled.
+# This may be replaced when dependencies are built.
