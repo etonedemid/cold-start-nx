3e739264bd4e3c875a1a77c33905326c15ec6e70
etonedemid <3damir0n3@gmail.com>
2026-03-08 23:26:58 -0300

    release: v0.9.9 dedicated host migration and transfer

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "0.9.8"
+        versionName = "0.9.9"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
