07365534667a2870fc45e210a8c00965581334c0
etonedemid <3damir0n3@gmail.com>
2026-03-14 11:15:09 -0300

    Remove Switch self-update dead code; add axe wall-hit + kill-bonus rumble

--- source/main.cpp
+++ source/main.cpp
@@ -69,10 +69,6 @@ int main(int argc, char* argv[]) {
     printf("COLD START launching...\n");
 
     Game game;
-#ifdef __SWITCH__
-    if (argc > 0 && argv[0] && argv[0][0] != '\0')
-        game.setNroPath(argv[0]);
-#endif
     if (dedicated) {
         game.configureDedicatedServer(dedicatedPort, dedicatedMaxPlayers,
                                       dedicatedPassword, dedicatedName);
